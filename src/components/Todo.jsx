a25f0886ab91effdc8c7ce096999e41ecf6da3cb
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2026-07-16 18:15:19 +0600

    enable deleting todo's with thunk

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,8 @@
 import { useDispatch } from "react-redux";
 import cancelImage from "../assets/images/cancel.png";
-import { deleted } from "../redux/todos/actions";
 import updateStatus from "../redux/todos/thunk/updateStatus";
 import updateColor from "../redux/todos/thunk/updateColor";
+import deleteTodo from "../redux/todos/thunk/deleteTodo";
 
 export default function Todo({ todo }) {
   const dispatch = useDispatch();
@@ -17,12 +17,12 @@ export default function Todo({ todo }) {
   };
 
   const handleDelete = (todoId) => {
-    dispatch(deleted(todoId));
+    dispatch(deleteTodo(todoId));
   };
   return (
     <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
       <div
-        className={`rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${
+        className={`relative rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${
           completed && "border-green-500 focus-within:border-green-500"
         }`}
       >
